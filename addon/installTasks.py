82b6f16718f28c433410ad1b69e3b5c33a0acbfe
Jose Manuel Delicado <jmdaweb@hotmail.com>
2017-05-17 21:50:23 +0200

    Added license information inside Python source files. Added COPYING.txt file

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,3 +1,8 @@
+#Enhanced Aria addon for NVDA
+#This file is covered by the GNU General Public License.
+#See the file COPYING.txt for more details.
+#Copyright (C) 2017 Jose Manuel Delicado <jmdaweb@hotmail.com>
+
 import config
 def onInstall():
 	defaults = {
