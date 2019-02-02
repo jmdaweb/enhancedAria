6ece1303b1657f8aac5556476da7aa6c5dea4d00
Jose Manuel Delicado <jmdaweb@hotmail.com>
2019-02-02 11:32:38 +0100

    Updated manifest keys with latest requirements for compatibility flags

--- buildVars.py
+++ buildVars.py
@@ -23,9 +23,9 @@ It has support for extra aria landmarks which are not included by default in NVD
 	"addon_url" : "https://addons.nvda-project.org/addons/enhancedAria.en.html",
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3")
-	"addon_minimumNVDAVersion" : "2017.3",
+	"addon_minimumNVDAVersion" : "2017.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2019.1",
+	"addon_lastTestedNVDAVersion" : "2019.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : "stable",
 }
